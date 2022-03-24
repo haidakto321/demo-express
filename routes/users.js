e9617f02134c4cf8a9099a2ac72f34f0389be791
haiks-2382 <kieu.son.hai@sun-asterisk.com>
2022-03-24 11:26:44 +0700

    Init db and crud api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../service/db');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('', async (req, res) => {
+  db.getUsers(req, res)
+});
+
+router.get('/:id', async (req, res) => {
+  db.getUserById(req, res)
+});
+
+router.post('', async (req, res) => {
+  db.createUser(req, res)
+});
+
+router.put('/:id', async (req, res) => {
+  db.updateUser(req, res)
+});
+
+router.delete('/:id', async (req, res) => {
+  db.deleteUser(req, res)
 });
 
 module.exports = router;
